0cd6501f62c119f13daecf7c6ee494bea1e2373f
Arfulmcdarful <blahaha>
2016-06-10 09:45:55 -0600

    Updated Transaction class to a work version

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,4 +1,42 @@
+import java.time.LocalDateTime;
+
+//Provided Transaction class//
 
 public class Transaction {
 
+	private LocalDateTime transactionTime;
+	private double amount;
+	private String description;
+	
+	public Transaction(LocalDateTime transactionTime, double amount, String description){
+		
+		this.transactionTime = transactionTime;
+		this.amount = amount;
+		this.description = description;
+		
+	}
+	
+	public Transaction(double amount, String description){
+		
+		this.amount = amount;
+		this.description = description;
+		
+	}
+	
+	public LocalDateTime getTransactionTime() {
+		
+		return transactionTime;
+		
+	}
+	public double getAmount() {
+		
+		return amount;
+		
+	}
+	public String getDescription() {
+		
+		return description;
+		
+	}
+	
 }
